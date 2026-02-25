28e952089152f2979a4da6f4ad9c14edff818f84
Amit-Mahato-0001 <amitmahatocky909@gmail.com>
2026-02-25 15:34:20 +0530

    colorBends bg webgl added

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,8 +1,28 @@
+import ColorBends from "../ColorBends";
+
 const Hero = () => {
 
     return(
-        <section className="flex min-h-[60vh] items-center">
-            <div className="mx-auto max-w-6xl px-4">
+        <section className="relative flex min-h-[50vh] items-center">
+
+            <div className="absolute inset-0">
+                <ColorBends
+                colors={["#ff0000", "#ff7a00", "#ffd500", "#0066ff"]}
+                rotation={120}
+                speed={0.2}
+                scale={3.5}
+                frequency={1}
+                warpStrength={1}
+                mouseInfluence={1}
+                parallax={0.5}
+                noise={0.1}
+                transparent
+                autoRotate={0}
+            />
+            </div>
+
+
+            <div className="mx-auto max-w-6xl px-4 relative z-10">
                 <p className="mb-4 text-sm text-[var(--color-muted)]">
                     Namaste, I'm Amit Kr. Mahato.
                 </p>
