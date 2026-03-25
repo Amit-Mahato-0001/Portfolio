0ac0b0925b5f1a5e161a8bc6814a080c7c383c90
Amit-Mahato-0001 <amitmahatocky909@gmail.com>
2026-03-25 23:16:51 +0530

    webgl removed

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,29 +1,17 @@
-import ColorBends from "../ColorBends";
-
 const Hero = () => {
 
     return(
+
         <section className="relative flex min-h-[50dvh] items-center px-4">
 
             <div className="absolute inset-0">
-                <ColorBends
-                colors={["#ff0000", "#ff7a00", "#ffd500", "#0066ff"]}
-                rotation={120}
-                speed={0.2}
-                scale={3.5}
-                frequency={1}
-                warpStrength={1}
-                mouseInfluence={1}
-                parallax={0.5}
-                noise={0.1}
-                transparent
-                autoRotate={0}
-            />
+                <img src="/images/train.webp"
+                className="h-full w-full object-cover object-center"/>
             </div>
 
 
             <div className="mx-auto max-w-6xl relative z-10">
-                <p className="mb-2 text-sm text-[var(--color-muted)]">
+                <p className="text-sm">
                     Namaste, I'm Amit Kr. Mahato.
                 </p>
 
@@ -32,6 +20,7 @@ const Hero = () => {
                 </h1>
             </div>
         </section>
+        
     )
 };
 
