d1fee2da17c20a0a1a8023953bce3077193742e4
Amit-Mahato-0001 <amitmahatocky909@gmail.com>
2026-04-22 23:54:18 +0530

    projects img updates

--- src/components/experiments/experiments.data.ts
+++ src/components/experiments/experiments.data.ts
@@ -11,7 +11,7 @@ export const EXPERIMENTS: Experiment[] = [
         id: "experiment01",
         title: "Backend Performance Optimization",
         description: "Optimized backend performance by increasing throughput from ~9 to ~9,700+ req/sec using caching, clustering, and load testing, while reducing latency from ~1000ms to ~1ms and identifying key scalability bottlenecks in Redis-based systems.",
-        image: "/images/Scaling.png",
+        image: "/images/Scaling.webp",
         href: "https://github.com/Amit-Mahato-0001/Scaling-API",
     },
 ];
