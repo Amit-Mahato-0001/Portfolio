868e8fae80eafa6e2a8ccf0b8706822eb47a9c9e
Amit-Mahato-0001 <amitmahatocky909@gmail.com>
2026-05-09 23:36:17 +0530

    removed about part

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,44 +1,15 @@
-import { Dispatch, SetStateAction } from "react";
 import resume from "../../assets/resume.pdf";
 
-type ViewType = "home" | "about";
-
-interface NavbarProps {
-  setView: Dispatch<SetStateAction<ViewType>>;
-}
-
-const Navbar = ({ setView }: NavbarProps) => {
+const Navbar = () => {
   return (
     <nav>
       <div className="flex items-center justify-between px-4 py-6">
-        <div
-          className="text-lg font-medium tracking-wide cursor-pointer"
-          onClick={() => setView("home")}
-        >
-          A.M.
-        </div>
+        <div className="text-lg font-medium tracking-wide cursor-pointer">A.M.</div>
 
         <div className="flex items-center gap-6">
-          <button
-            onClick={() => setView("home")}
-            className="text-lg text-gray-400 hover:text-white transition"
-          >
-            Work
-          </button>
+          <button className="text-lg text-gray-400 hover:text-white transition">Work</button>
 
-          <button
-            onClick={() => setView("about")}
-            className="text-lg text-gray-400 hover:text-white transition"
-          >
-            About
-          </button>
-
-          <a
-            href={resume}
-            className="text-lg text-gray-400 hover:text-white transition"
-          >
-            Resume
-          </a>
+          <a href={resume} className="text-lg text-gray-400 hover:text-white transition">Resume</a>
         </div>
       </div>
     </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,22 @@
-import { useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 import Navbar from "../components/navbar/Navbar";
 import Hero from "../components/hero/Hero";
 import WorkSection from "../components/work/WorkSection";
 import ExperimentsSection from "../components/experiments/ExperimentsSection";
-import About from "../components/about/About";
 import Footer from "../components/footer/Footer";
 
-type ViewType = "home" | "about";
-
 const Home = () => {
-  const [view, setView] = useState<ViewType>("home");
-
   return (
     <MainLayout>
+      <Navbar />
 
-      <Navbar setView={setView} />
-
-      {view === "home" && (
-        <>
-          <Hero />
-          <WorkSection />
-          <ExperimentsSection />
-        </>
-      )}
-
-      {view === "about" && <About />}
+      <>
+        <Hero />
+        <WorkSection />
+        <ExperimentsSection />
+      </>
 
       <Footer />
-
     </MainLayout>
   );
 };
